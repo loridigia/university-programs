f987e66736018affe332329ec6852560cd47469f
loridigia <loridigia@gmail.com>
2019-04-08 17:13:44 +0200

    just a little clean

--- MIPS/Tree-Height.asm
+++ MIPS/Tree-Height.asm
@@ -8,39 +8,31 @@ c: .word 7, d, e
 d: .word 21, 0, 0
 e: .word 2, 0, 0
 
-.text 
+.text
 lui $a0, 0x1001
 
-cavallo: bne $a0, $zero, ric
+end: bne $a0, $zero, ric
 li $v0, 0
 jr $ra
 
 
 
-ric: 
+ric:
 addi $sp, $sp, -12
 sw $ra, 0($sp)
 sw $a0, 4($sp)
 lw $a0, 4($a0)
-jal cavallo
+jal end
 sw $v0, 8($sp)
 lw $a0, 4($sp)
-lw $a0, 8($a0) 
-jal cavallo
+lw $a0, 8($a0)
+jal end
 lw $t0, 8($sp)
 slt $t1, $v0, $t0
-beq $t1, $zero, sk  # v0 è gia il piu grande 
+beq $t1, $zero, sk  # v0 è gia il piu grande
 move $v0, $t0
 
 sk: addi $v0, $v0, 1
     lw $ra, 0($sp)
     addi $sp, $sp, 12
     jr $ra
-
-
-#fatto dal prof:
-
-
-
-
-
